a172b663891292fbd8cfa8103d8ef8deb5f032b4
Your Name <you@example.com>
2021-04-13 12:26:23 +0400

    start

---
+++ errmech/errmech/static/js/main.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+  $('.homepage .slider').slick({
+    fade: true,
+    speed: 800,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    dots: true,
+    draggable: false, 
+    responsive: [
+      {
+        breakpoint: 1140,
+        settings: {
+          arrows: false,
+
+        }
+      },
+    ]
+  });
+
+  $('.header-burger').click(function() {
+    $(this).toggleClass('active');
+    $('.menu').slideToggle(400);
+  });
+
+  $('#theme').click(function() {
+    $(this).find('.theme__checkbox').toggleClass('dark')
+  });
+
+  $('.images-small__image-wrapper').click(function() {
+    let imgLink = $(this).find('img').attr('src');
+    $('.images-big__image').attr('src', imgLink);
+  });
+
+  $('.item-switches__item').click(function() {
+    if (!$(this).hasClass('disabled')) {
+      $('.item-switches__item').removeClass('active');
+      $(this).addClass('active');
+    }
+  });
+
+  $('.checkbox').click(function() {
+    $(this).toggleClass('active');
+  });
+});
